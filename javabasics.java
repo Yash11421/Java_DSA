a1e74d70066457182e03b50ab16666bef320b1b3
YASH <yashbhanushali142@gmail.com>
2022-11-18 15:02:28 +0530

    first commit

---
+++ javabasics.java
@@ -0,0 +1,36 @@
+/* public class javabasics{
+  public static void main(String args[]){
+    System.out.println("Hello World");
+  }
+}
+*/
+
+/* public class javabasics{
+  public static void main(String args[]){
+    System.out.println("* * *");
+    System.out.println("* * ");
+    System.out.println("* ");
+  }
+} */
+
+/* import java.util.*;
+
+public class javabasics{
+  public static void main(String args[]){
+    Scanner S= new Scanner(System.in);
+    int a = S.nextInt();
+    int b = S.nextInt();
+    int sum = a + b;
+    System.out.println(sum);
+  }
+} */
+
+import java.util.*;
+ public class javabasics{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    float b = a ;
+    System.out.println(b);
+  }
+ }
